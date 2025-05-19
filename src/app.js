c2017b30f07212e9055d89fdd0e65bc933f68c16
jeanc521 <jeanromoaldoc@gmail.com>
2025-05-19 15:25:48 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import routes from './routes.js'
+import cors from 'cors'
+import express from 'express'
+
+// Inciar o express
+const app = express();
+
+/* Definir as regras do aservidor 
+Meio de comunicação e um json */
+app.use(express.json());
+
+// Definir como os dados vao ser intrepertado
+app.use(express.urlencoded({extended:  true}))
+
+/* Definir o cors - middlewaee para aplicações node.js
+Que permite ou restinge requisiçoes entre origens 
+ ou seja requisiçoes croos-oringin */
+ app.use(cors())
+
+// Deinir as rotas 
+app.use(routes)
+
+export default app;
