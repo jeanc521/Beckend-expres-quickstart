6f522657c040267fcf83b9832c59053f87460052
jeanc521 <jeanromoaldoc@gmail.com>
2025-05-26 09:54:33 -0300

    Add files via upload

--- src/controllers/list.ts
+++ src/controllers/list.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "../../generated/prisma";
-import { create } from "domain";
+
 
 const prisma = new PrismaClient()
 
@@ -8,5 +8,22 @@ export default{
     create : async(req: Request, res: Response) => {
         const list = await prisma.list.create({data: req.body})
         return res.status(201).json(list)
-    }
+    },
+
+    read : async(req: Request, res: Response) => {
+        const list = await prisma.list.findMany({select : {id: true, description: true, price: true, quanity: true, image: true}})
+        return res.status(200).json(list)
+    },
+
+    update : async(req: Request, res: Response) => {
+        const id = req.params.id
+        const list = await prisma.user.update({data: req.body, where: {id: + id}})
+        return res.status(200).json(list)
+    },
+
+    delete: async(req: Request, res: Response) => {
+        const id = req.params.id
+        const list = await prisma.list.delete({where: {id: + id}})
+        return res.status(200).json(list)
+    },
 }
--- src/routes.ts
+++ src/routes.ts
@@ -71,4 +71,11 @@ routes.put("/user/:id", (req: Request, res: Response) : any => userController.up
 routes.delete("/user/:id", (req: Request, res:  Response): any => userController.delete(req, res))
 routes.post("/login", (req: Request, res: Response): any => userController.login(req, res))
 
+
+routes.post("/list", (req: Request, res: Response): any => userController.create(req, res))
+routes.get("/list", (req: Request, res: Response): any => userController.read(req, res))
+routes.put("/list/:id", (req: Request, res: Response): any => userController.update(req, res))
+routes.delete("/list/:id", (req: Request, res: Response): any => userController.delete(req, res))
+
+
 export default routes;
